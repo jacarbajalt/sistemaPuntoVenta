c8e62dc50035b7a7aaad555e22181edf5fc6c485
Juan Antonio Carbajal T. <3519110055@uth.edu.mx>
2024-08-05 17:25:57 -0600

    Borrado de Carpeta no Utilizada

--- web/js/general/inicio.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    Swal.fire({
-        title: '¡Bienvenido!',
-        text: 'Esto es una alerta que aparece automáticamente al cargar la página.',
-        icon: 'info',
-        confirmButtonText: 'Cerrar'
-    });
-});
